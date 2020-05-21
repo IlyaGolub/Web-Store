549d11656fbb7f4cf6b443e6729769f2e67d1257
IlyaGolub <47134681+IlyaGolub@users.noreply.github.com>
2020-05-22 04:03:13 +1000

    Нужно добить контроллер

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Web_Store.Models;
 
 namespace Web_Store.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees= new Employee() { 
+
+        }
+
+        public IActionResult EmployeeDatails(int id)
+        {
+            var employee = _Employees.FirstOrDefault(x => x.Id == id);
+            if (employee is null) return NotFound();
+        }
         public IActionResult Index()
         {
-            return Content("First controller action result");
+        ViewBag.title = "Hello world";
+            return View(_Employees);
         }
         public IActionResult AnotherAction()
         {
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Store.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Patronimic { get; set; }
+        public string Age { get; set; }
+
+
+    }
+}
